a4b7c128bf21a97a186308aec8900e65ac91f3f0
Dmitry Guryaev <guryaidemon@gmail.com>
2022-01-10 11:13:36 +0300

    Отредактировал ссылки

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('group_list/', views.group_posts, name='group_list'),
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,11 +6,25 @@ def index(request):
     """Главная страница."""
 
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Это главная страница проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
 
 
 def group_posts(request):
     """Страница группы"""
     
     template = 'posts/group_list.html'
-    return render(request, template)
+    title = 'Здесь будет информация о группах проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
