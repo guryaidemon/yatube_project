8521f949529ae0c31b48df43bf3429f7e4a5bce5
Dmitry Guryaev <guryaidemon@gmail.com>
2022-01-14 10:09:32 +0300

    добавил константу

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Post, Group
 
+EMPTY_VALUE: str = '-пусто-'
+
 
 class PostAdmin(admin.ModelAdmin):
 
@@ -15,7 +17,7 @@ class PostAdmin(admin.ModelAdmin):
     list_editable = ('group',)
     search_fields = ('text',)
     list_filter = ('pub_date',)
-    empty_value_display = '-пусто-'
+    empty_value_display = EMPTY_VALUE
 
 
 class GroupAdmin(admin.ModelAdmin):
@@ -26,8 +28,8 @@ class GroupAdmin(admin.ModelAdmin):
         'slug',
         'description',
     )
-    prepopulated_fields = {"slug": ("title",)}
-    empty_value_display = '-пусто-'
+    prepopulated_fields = {'slug': ('title',)}
+    empty_value_display = EMPTY_VALUE
 
 
 admin.site.register(Post, PostAdmin)
